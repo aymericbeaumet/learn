9394b98f5da3c4e33fc8ee8f0e410c2e90e82158
Aymeric Beaumet <hi@aymericbeaumet.com>
2022-04-20 19:13:58 +0200

    add a conclusion

--- scripts/generate-lessons-index.sh
+++ scripts/generate-lessons-index.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find ./static/data/lessons -type f -exec basename -s .md {} \; | jq -R -s -c 'split("\n")[:-1]' >./src/lib/assets/lessons-index.json
+find ./static/data/lessons -type f -exec basename -s .md {} \; | sort -n | jq -R -s -c 'split("\n")[:-1]' >./src/lib/assets/lessons-index.json
--- src/routes/lessons/index.svelte
+++ src/routes/lessons/index.svelte
@@ -1,8 +1,9 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import lessonsIndex from '$lib/assets/lessons-index.json';
 
 	onMount(() => {
-		goto('/lessons');
+		goto(`/lessons/${lessonsIndex[0]}`);
 	});
 </script>
